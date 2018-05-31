0590feb5143577f6668c1e0fcda60a47c59b20e0
Al Kent <al.kent@mastercard.com>
2018-05-31 16:48:13 -0500

    starting to add support for xml serialization and response parsing

--- lib/src/main/java/com/mastercard/ats/common/Util.kt
+++ lib/src/main/java/com/mastercard/ats/common/Util.kt
@@ -1,5 +1,7 @@
 package com.mastercard.ats.common
 
+import org.xmlpull.v1.XmlSerializer
+import java.io.StringWriter
 import java.nio.ByteBuffer
 
 fun Int.bytes(): ByteArray {
@@ -7,3 +9,71 @@ fun Int.bytes(): ByteArray {
             .putInt(this)
             .array()
 }
+
+//  XML generation by code
+fun XmlSerializer.document(docName: String = "UTF-8",
+                           xmlStringWriter: StringWriter = StringWriter(),
+                           init: XmlSerializer.() -> Unit): String {
+    startDocument(docName, null)
+    xmlStringWriter.buffer.setLength(0) //  refreshing string writer due to reuse
+    setOutput(xmlStringWriter)
+    init()
+    endDocument()
+    return xmlStringWriter.toString()
+}
+
+//  element
+fun XmlSerializer.element(name: String, init: XmlSerializer.() -> Unit) {
+    startTag("", name)
+    init()
+    endTag("", name)
+}
+
+//  element with attribute & content
+fun XmlSerializer.element(name: String,
+                          content: String,
+                          init: XmlSerializer.() -> Unit) {
+    startTag("", name)
+    init()
+    text(content)
+    endTag("", name)
+}
+
+//  element with content
+fun XmlSerializer.element(name: String, content: String) =
+        element(name) {
+            text(content)
+        }
+
+//  attribute
+fun XmlSerializer.attribute(name: String, value: String) =
+        attribute("", name, value)
+
+
+
+//val xmlSerializer = Xml.newSerializer()
+//val xmlString = xmlSerializer.document {
+//    element("Movies") {
+//        element("row") {
+//            attribute("no", "1")
+//            element("FL", "6000000066015") {
+//                attribute("val", "TicketId")
+//            }
+//            element("FL", "Dunkirk") {
+//                attribute("val", "MovieName")
+//            }
+//            element("FL") {
+//                attribute("val", "TimeLog")
+//                element("row") {
+//                    attribute("no", "1")
+//                    element("FL", "23/01/2018") {
+//                        attribute("val", "date")
+//                    }
+//                    element("FL", "08:00") {
+//                        attribute("val", "startTime")
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}

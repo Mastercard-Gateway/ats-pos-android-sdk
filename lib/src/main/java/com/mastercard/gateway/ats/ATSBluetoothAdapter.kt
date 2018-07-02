e7a93e239c948a7b70fb9bae859b89f6a8c77672
Al Kent <al.kent@mastercard.com>
2018-07-02 16:31:46 -0500

    cleaning up contract for setting port / btDevice

--- lib/src/main/java/com/mastercard/gateway/ats/ATSBluetoothAdapter.kt
+++ lib/src/main/java/com/mastercard/gateway/ats/ATSBluetoothAdapter.kt
@@ -2,68 +2,101 @@ package com.mastercard.gateway.ats
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import com.mastercard.gateway.common.*
 import java.io.Closeable
 import java.net.Socket
 import java.util.*
 
-object ATSBluetoothAdapter : Closeable {
+object ATSBluetoothAdapter {
 
-    private val CONNECTION_ATTEMPTS = 3
+    private const val CONNECTION_ATTEMPTS = 3
 
-    internal var bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    @JvmStatic
+    var port: Int? = null
+
+    @JvmStatic
+    var bluetoothDevice: BluetoothDevice?
+        get() {
+            return bluetoothSocketClient?.device
+        }
+        set (value) {
+            bluetoothSocketClient = if (value != null) {
+                BluetoothSocketClient(device = value, secure = true).apply {
+                    addCallback(bluetoothSocketCallback)
+                }
+            } else {
+                bluetoothSocketClient?.close()
+                null
+            }
+        }
+
     internal var bluetoothSocketClient: BluetoothSocketClient? = null
+//    internal var bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
     internal var socketClient: SocketClient? = null
-    internal lateinit var socketServer: SocketServer
+    internal var socketServer: SocketServer? = null
 
     private val bluetoothSocketCallback = BluetoothSocketCallback()
     private val incomingSocketCallback = SocketCallbacks()
 
-    @JvmStatic
-    fun init(port: Int) {
+//    @JvmStatic
+//    fun init(port: Int) {
+//
+//        if (::socketServer.isInitialized) {
+//            throw IllegalStateException("ATSBluetoothAdapter has already been initialized")
+//        }
+//
+//        socketServer = SocketServer(port).apply {
+//            addCallback(SocketServerCallback())
+//        }
+//    }
 
-        if (::socketServer.isInitialized) {
-            throw IllegalStateException("ATSBluetoothAdapter has already been initialized")
+//    @JvmStatic
+//    @JvmOverloads
+//    fun setBluetoothDevice(deviceName: String?, secure: Boolean = true) {
+//        if (!deviceName.isNullOrEmpty()) {
+//            "Looking for Bluetooth device: $deviceName".log(this)
+//            val bondedDevices = bluetoothAdapter.bondedDevices
+//            val device = try {
+//                bondedDevices.first { deviceName == it.name }
+//            } catch (exception: NoSuchElementException) {
+//                null
+//            }
+//
+//
+//
+//            when (device) {
+//                null -> throw RuntimeException("No device found with name: $deviceName")
+//                else -> {
+//                    bluetoothSocketClient = BluetoothSocketClient(device = device, secure = secure).apply {
+//                        addCallback(bluetoothSocketCallback)
+//                    }
+//                    "Found: $deviceName".log(this)
+//                }
+//            }
+//        } else {
+//            // Close and null out bluetooth socket client if name is null
+//            bluetoothSocketClient?.close()
+//            bluetoothSocketClient = null
+//        }
+//    }
+
+    @JvmStatic
+    fun start() {
+        if (port == null) {
+            throw IllegalStateException("A valid port must be set")
+        } else if (bluetoothDevice == null) {
+            throw IllegalStateException("A valid bluetooth device must be set")
         }
 
-        socketServer = SocketServer(port).apply {
+        socketServer = SocketServer(port!!).apply {
             addCallback(SocketServerCallback())
         }
     }
 
     @JvmStatic
-    @JvmOverloads
-    fun setBluetoothDevice(deviceName: String?, secure: Boolean = true) {
-        if (!deviceName.isNullOrEmpty()) {
-            "Looking for Bluetooth device: $deviceName".log(this)
-            val bondedDevices = bluetoothAdapter.bondedDevices
-            val device = try {
-                bondedDevices.first { deviceName == it.name }
-            } catch (exception: NoSuchElementException) {
-                null
-            }
-
-
-
-            when (device) {
-                null -> throw RuntimeException("No device found with name: $deviceName")
-                else -> {
-                    bluetoothSocketClient = BluetoothSocketClient(device = device, secure = secure).apply {
-                        addCallback(bluetoothSocketCallback)
-                    }
-                    "Found: $deviceName".log(this)
-                }
-            }
-        } else {
-            // Close and null out bluetooth socket client if name is null
-            bluetoothSocketClient?.close()
-            bluetoothSocketClient = null
-        }
-    }
-
-
-    override fun close() {
-        socketServer.close()
+    fun stop() {
+        socketServer?.close()
         bluetoothSocketClient?.close()
     }
 
@@ -114,8 +147,8 @@ object ATSBluetoothAdapter : Closeable {
 
         override fun onRead(bytes: ByteArray) {
             // Pass incoming bytes from ATS to the bluetooth device
-           /* """"From ATS:
-                |${Base64.getEncoder().encode(bytes)}""".log(this)*/
+            /* """"From ATS:
+                 |${Base64.getEncoder().encode(bytes)}""".log(this)*/
             bluetoothSocketClient?.write(bytes)
         }
 

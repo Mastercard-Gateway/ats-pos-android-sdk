b3497839c268010fcb1a3b41fa5484a1355bae16
Nitin Shambhu <nitin_shambhu@mastercard.com>
2018-07-10 17:18:34 +0100

    Added method to acquire device.

--- sample/src/main/java/com/mastercard/gateway/sample/SelectActionActivity.java
+++ sample/src/main/java/com/mastercard/gateway/sample/SelectActionActivity.java
@@ -2,10 +2,16 @@ package com.mastercard.gateway.sample;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.mastercard.gateway.ats.ATSClient;
+import com.mastercard.gateway.ats.domain.ServiceRequest;
+import com.mastercard.gateway.ats.domain.ServiceRequestType;
 
 public class SelectActionActivity extends Activity {
 
@@ -41,5 +47,26 @@ public class SelectActionActivity extends Activity {
                 startActivity(new Intent(SelectActionActivity.this, AmountActivity.class), options);
             }
         });
+
+        findViewById(R.id.button_acquire_device).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                acquireDevice();
+            }
+        });
     }
+
+    void acquireDevice() {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        ServiceRequest request = new ServiceRequest();
+        request.setRequestType(ServiceRequestType.AcquireDevice);
+        request.setWorkstationID(preferences.getString("ATS_WORKSTATION_ID", ""));
+        request.setRequestID("9786");
+
+        ATSClient atsClient = ((SampleApplication) getApplication()).getAtsClient();
+        atsClient.sendMessage(request);
+    }
+
 }
